b038256e607156022bc533f0980826c435177c1a
Andrew Park <acpark123@gmail.com>
2020-06-11 17:46:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,6 @@ def reverse_each_word(sentence)
   split_sentence = sentence.split
   reverved_sentence = ""
   split_sentence.collect do |word|
-
     reverved_sentence << "#{word.reverse.to_s} "
   end
   reverved_sentence[0...-1]
